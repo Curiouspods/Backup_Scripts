ffd48c10142bec65e36426117a833155c3140285
Vtex.ai <Curiouspods@gmail.com>
2025-07-04 23:49:37 +0530

    Update Backup.sh

--- Backups/Backup.sh
+++ Backups/Backup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+db="MongoDB"
+Backup_time=$(date +"%Y%m%d_%H%M%S")
+
+folder="${db}_${Backup_time}"
+mkdir "$folder"
+
+mongodump --uri="mongodb+srv://AdminUser:55chai%%1001@vtex2025-prod.ycmrama.mongodb.net/" --out "$folder"
