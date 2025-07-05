f7b48b051fed3fda0bfa2d990f8f5abb4c55cccf
Vtex.ai <Curiouspods@gmail.com>
2025-07-05 13:47:12 +0530

    Update Backup.sh

--- Backups/Backup.sh
+++ Backups/Backup.sh
@@ -6,6 +6,8 @@ Backup_time=$(date +"%Y%m%d_%H%M%S")
 folder="${db}_${Backup_time}"
 mkdir "$folder"
 
+# Run mongodump
 mongodump --uri="mongodb+srv://Adminuser:vFa4GYtmHnpLZQQK@vtexai-prod.carxbxj.mongodb.net/?retryWrites=true&w=majority&appName=VtexAI-Prod" --out "$folder"
 
-rclone copy $folder gdrive:/Backups/
+# Upload to Google Drive 'DB_BACKUPS' folder
+rclone copy -v "$folder" gdrive:/DB_BACKUPS/
